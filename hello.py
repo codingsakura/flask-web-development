ca5b80364a6910f05f321be7bf1dcff00b8e4659
codingsakura <codingsakura@gmail.com>
2018-01-30 14:54:16 +0800

    6a
    need to create new_user.html & new_user.txt as the path

--- hello.py
+++ hello.py
@@ -9,6 +9,7 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
+from flask_mail import Mail, Message
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
@@ -16,12 +17,21 @@ app.config['SQLALCHEMY_DATABASE_URI'] = \
 	'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 #SQLALCHEMY_TRACK_MODIFICATIONS = False
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
+app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <codingsakura@gmail.com>'
+app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
 
 db = SQLAlchemy(app)
 app.config['SECRET_KEY'] = 'hard to guess string'
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+mail = Mail(app)
 
 class NameForm(FlaskForm):
 	name = StringField('What is your name?', validators = [Required()])
@@ -55,6 +65,8 @@ def index():
 			user = User(username = form.name.data)
 			db.session.add(user)
 			session['known'] = False
+			if app.config['FLASKY_ADMIN']:
+				send_email(app.config['FLASKY_ADMIN'], 'New User', 'mail/new_user', user = user)
 		else:
 			session['known'] = True
 		session['name'] = form.name.data
@@ -79,6 +91,12 @@ def make_shell_context():
 manager.add_command('shell', Shell(make_context = make_shell_context))
 migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
+
+def send_email(to, subject, template, **kwargs):
+	msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject, sender = app.config['FLASKY_MAIL_SENDER'], recipients = [to])
+	msg.body = render_template(template + '.txt', **kwargs)
+	msg.html = render_template(template + '.html', **kwargs)
+	mail.send(msg)
 
 if __name__=='__main__':
 	manager.run()
